b8411498a18c3020d0d7074cbf1de44540bd4bf3
Co985 <82610576+Co985@users.noreply.github.com>
2022-06-22 06:55:36 -0500

    Add files via upload

---
+++ Dragons/Dragon.java/cave.java
@@ -0,0 +1,10 @@
+package genspark.assignments;
+
+
+public class cave{
+
+    public static void Talk(){
+
+        System.out.println("One cave have a friendly dragon. The other cave the dragon will eat you.");
+    }
+}
---
+++ Dragons/Dragon.java/friendly.java
@@ -0,0 +1,13 @@
+package genspark.assignments;
+
+public class friendly extends cave{
+
+    public static void Talk(){
+
+
+        System.out.println("You approach the cave...");
+        System.out.println("It is very bright and inviting...");
+        System.out.println("A very friendly dragon says Hello!");
+        System.out.println("The dragon gives you lots of treasure!");
+    }
+}
